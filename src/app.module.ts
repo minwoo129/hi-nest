efe8ebce7aef8973f7968b40717eca5201b3b7d5
minwoo129 <key0129mw@naver.com>
2026-06-21 12:55:33 +0900

    controller, service 예시 코드 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AppService } from './app.service';
       envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할(ex, nodejs의 express)
+  providers: [AppService], // 비즈니스 로직을 담당(ex, 데이터베이스 처리, 로직 처리)
 })
 export class AppModule {}
