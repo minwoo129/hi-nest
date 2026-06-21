27118b61c995502d04010b3fedbb1d52882e9946
minwoo129 <key0129mw@naver.com>
2026-06-21 13:55:04 +0900

    app controller 재생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app/app.controller';
 
 @Module({
   imports: [
@@ -9,8 +9,9 @@ import { MoviesService } from './movies/movies.service';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
     }),
+    MoviesModule,
   ],
-  controllers: [MoviesController], // URL을 가져오고 함수를 실행하는 역할(ex, nodejs의 express)
-  providers: [MoviesService], // 비즈니스 로직을 담당(ex, 데이터베이스 처리, 로직 처리)
+  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할(ex, nodejs의 express)
+  providers: [], // 비즈니스 로직을 담당(ex, 데이터베이스 처리, 로직 처리)
 })
 export class AppModule {}
