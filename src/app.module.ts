a7eeb04e254fb705c641a187accd40f66ca3e3d1
minwoo129 <key0129mw@naver.com>
2026-06-21 13:13:36 +0900

    moviecontroller 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [
@@ -10,7 +9,7 @@ import { AppService } from './app.service';
       envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
     }),
   ],
-  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할(ex, nodejs의 express)
-  providers: [AppService], // 비즈니스 로직을 담당(ex, 데이터베이스 처리, 로직 처리)
+  controllers: [MoviesController], // URL을 가져오고 함수를 실행하는 역할(ex, nodejs의 express)
+  providers: [], // 비즈니스 로직을 담당(ex, 데이터베이스 처리, 로직 처리)
 })
 export class AppModule {}
